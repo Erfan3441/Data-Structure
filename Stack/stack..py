e181a8ac552ec8b2df1727d7304ed98fffd42220
erfan <erfanf3441@gmail.com>
2023-11-22 12:56:54 +0330

    upload stack code

---
+++ Stack/stack..py
@@ -0,0 +1,30 @@
+class Stack:
+
+    def _init_(self):
+        self.values = []
+
+
+        def push(self, value):
+            self.values.append(value)
+
+
+        def peek(self):
+                if not self.Is_empty():
+                    return self.values[-1]
+
+                else:
+                    return False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
