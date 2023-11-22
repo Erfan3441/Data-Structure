829682a4c7b24198967dbd31d90f56795d8de57a
erfan <erfanf3441@gmail.com>
2023-11-22 15:21:44 +0330

    updated

--- Queue-Stack/Queue-Stack (1).py
+++ Queue-Stack/Queue-Stack (1).py
@@ -7,4 +7,18 @@ class Queue:
         self.enqueue_stack.append(value)
 
 
+    def dequeue(self):
+        if not self.dequeue_stack:
+            while self.enqueue_stack:
+                last_element1 = self.enqueue_stack[-1]
+                del self.enqueue_stack[-1]
+                self.dequeue_stack.append(last_element1)
+        if self.dequeue_stack:
+            last_element2 = self.dequeue_stack[-1]
+            del self.dequeue_stack[-1]
+            return last_element2
+        else:
+            raise IndexError("Queue is empty")
+
+
 
